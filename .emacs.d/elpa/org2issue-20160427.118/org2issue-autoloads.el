4b25e68da4b398ee46d2088e6eea9f92185ef9eb
Angel <roaguiangel@gmail.com>
2020-04-26 12:20:46 +0200

    cleaned files

--- .emacs.d/elpa/org2issue-20160427.118/org2issue-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; org2issue-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "org2issue" "org2issue.el" (23484 30014 496302
-;;;;;;  545000))
-;;; Generated autoloads from org2issue.el
-
-(autoload 'org2issue "org2issue" "\
-
-
-\(fn &optional DELETE)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; org2issue-autoloads.el ends here
--- .emacs.d/elpa/transient-20190812.1336/transient-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; transient-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("transient-pkg.el" "transient.el") (23890
-;;;;;;  33159 12157 641000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; transient-autoloads.el ends here
