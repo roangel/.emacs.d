968b3a87b784f8cf3e4820e78da53400180bc74c
Angel <roaguiangel@gmail.com>
2022-06-02 15:03:50 +0200

    added emacs config

--- configs/my_org_mode.el
+++ configs/my_org_mode.el
@@ -3,6 +3,7 @@
 (setq org-agenda-files (list "~/.org/my-org-files/work.org"
                              "~/.org/my-org-files/personal.org"))
 
+
 (setq org-agenda-span 10
       org-agenda-start-on-weekday nil
       org-agenda-start-day "-5d")
@@ -41,6 +42,14 @@
    (python . t)
    (C . t)))
 (setq org-babel-python-command "python3")
+
+(defun my-org-confirm-babel-evaluate (lang body)
+  (not (member lang '("elisp" "python"))))
+
+(setq org-confirm-babel-evaluate 'my-org-confirm-babel-evaluate)
+
+(add-hook 'LaTeX-mode-hook #'visual-line-mode)
+
 
 
 (provide 'my_org_mode)
